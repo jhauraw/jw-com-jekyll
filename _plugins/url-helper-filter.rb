33fc996c76e944b434996925df6f65ad55462b03
Jhaura Wachsman <jw@jhaurawachsman.com>
2013-04-18 11:12:38 -0700

    Comments username, Refactor plugin.

--- _plugins/url-helper-filter.rb
+++ _plugins/url-helper-filter.rb
@@ -1,47 +1,45 @@
 module Jekyll
   module Filters
-
-    def sub_baseurl(input)
-
-      # Use the value of @site.baseurl from _config.yml
-      # to_s() in case null or non-string to prevent errors
-      baseurl = @context.registers[:site].config['baseurl'].to_s
-
-      # Append @baseurl to Root-Relative URLs in templates
-      # to make a Root-Base-Relative URL. Looks for ="/ in
-      # href or src tags
-      input.gsub(/(href|src)="\//, '\1="' + baseurl + '/')
-    end
-
-    def sub_absurl(input)
+    def get_url
 
       # Use the value of @site.url from _config.yml
       # to_s() in case null or non-string to prevent errors
       url = @context.registers[:site].config['url'].to_s
+    end
 
+    def get_baseurl
+
+      # Use the value of @site.baseurl from _config.yml
+      # to_s() in case null or non-string to prevent errors
       baseurl = @context.registers[:site].config['baseurl'].to_s
+    end
+
+    def sub_baseurl(input)
+
+      # Append @baseurl to Root-Relative URLs in templates
+      # to make a Root-Base-Relative URL. Looks for ="/ in
+      # href or src tags
+      input.gsub(/(href|src)="\//, '\1="' + get_baseurl + '/')
+    end
+
+    def sub_absurl(input)
 
       # Append @url and @baseurl to Root-Relative URLs in
       # templates to make an Absolute URL. Looks for ="/ in
       # href or src tags.
-      input.gsub(/(href|src)="\//, '\1="' + url + baseurl + '/')
+      input.gsub(/(href|src)="\//, '\1="' + get_url + get_baseurl + '/')
     end
 
     def to_baseurl(input)
 
-      baseurl = @context.registers[:site].config['baseurl'].to_s
-
       # baseurl << input # wouldn't work, created a huge concat chain
-      input = baseurl + input
+      input = get_baseurl + input
     end
 
     def to_absurl(input)
 
-      url = @context.registers[:site].config['url'].to_s
-      baseurl = @context.registers[:site].config['baseurl'].to_s
-
       # url + baseurl << input
-      input = url + baseurl + input
+      input = get_url + get_baseurl + input
     end
   end
 end
